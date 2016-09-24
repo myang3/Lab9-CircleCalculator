5a57d443b6a470e97e799d7de5bb65a3e6f4f895
Mo Yang <moyang9192@gmail.com>
2016-09-23 20:12:43 -0400

    Add files via upload

--- Lab9 CircleCalculator/Program.cs
+++ Lab9 CircleCalculator/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Lab9_CircleCalculator
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
+            // private static double radius;
+            static string keepGoing;
+
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Welcome to the Circle Tester");
+
+                do
+                {
+                    Console.WriteLine("Enter Radius: ");
+                    double radius = double.Parse(Console.ReadLine());
+
+                    //double radius = new double();
+                    Circle C = new Circle(radius);
+
+                    Console.WriteLine("Circumference: " + C.getFormattedCircumference());
+                    Console.WriteLine("Area: " + C.getFormattedArea());
+
+                    Console.WriteLine("Continue? (y/n): ");
+                    keepGoing = Console.ReadLine();
+                } while (keepGoing.ToLower() == "y" || keepGoing.ToLower() == "yes");
+
+
+                {
+                    Console.WriteLine("Goodbye. You Created " + Circle.getObjectCount() + " Circle object (s).");
+
+                }
+
+                Console.ReadLine();
+            }
         }
     }
-}
+
+
+
