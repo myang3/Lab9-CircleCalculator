2da83899eefd780905b275f6177ad942d6eec7da
Mo Yang <moyang9192@gmail.com>
2016-09-24 21:21:28 -0400

    Add files via upload

---
+++ Lab9 CircleCalculator_Revised/Lab9 CircleCalculator/CircleApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_CircleCalculator
+{
+    class CircleApp
+    {
+        static string keepGoing;
+
+        static void Main(string[] args)
+        {
+            double radius;
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            do
+            {
+                Console.WriteLine("Enter radius: ");
+                radius = Validator.validateRadius();
+                Circle C = new Circle(radius);
+                Console.WriteLine("Circumference: " + C.getFormattedCircumference());
+                Console.WriteLine("Area: " + C.getFormattedArea());
+
+                Console.WriteLine("Continue? (y/n): ");
+                keepGoing = Console.ReadLine();
+            } while (keepGoing.ToLower() == "y" || keepGoing.ToLower() == "yes");
+
+
+            {
+                Console.WriteLine("Goodbye. You Created " + Circle.getObjectCount() + " Circle object (s).");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab9 CircleCalculator_Revised/Lab9 CircleCalculator/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_CircleCalculator
+{
+    public class Validator
+    {
+        public static object C { get; internal set; }
+
+        public static double validateRadius()
+        {
+            double radius;
+            do
+            {
+                
+                bool rinput = double.TryParse(Console.ReadLine(), out radius);
+                if (radius <= 0)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid radius: ");
+                }
+            } while (radius <= 0);
+
+            return radius;
+        }
+    }
+}
